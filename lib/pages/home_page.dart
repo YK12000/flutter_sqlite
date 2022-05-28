3f17a62fb91f586b881bddd51a9e0dc1db6ae69b
yk12000 <6279020+YK12000@users.noreply.github.com>
2022-05-28 22:53:41 +0900

    section2-4

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_sqlite/alarm.dart';
+import 'package:intl/intl.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -8,24 +10,52 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List<Alarm> alarmList = [
-    Alarm(alarmTime: DateTime.now())
+    Alarm(alarmTime: DateTime.now()),
+    Alarm(alarmTime: DateTime.now()),
   ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutterラボ'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      backgroundColor: Colors.black,
+      body: CustomScrollView(
+        slivers: [
+          CupertinoSliverNavigationBar(
+            backgroundColor: Colors.black,
+            largeTitle: Text('アラーム',style: TextStyle(color: Colors.white)),
+            trailing: GestureDetector(
+                child: Icon(Icons.add,color: Colors.orange,),
+                onTap: (){
+
+                },
             ),
-          ],
-        ),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                Alarm alarm = alarmList[index];
+                return Column(
+                  children: [
+                    if(index == 0) Divider(color: Colors.grey,height: 2),
+                    ListTile(
+                      title: Text(DateFormat('H:mm').format(alarm.alarmTime),
+                          style: TextStyle(color: Colors.white, fontSize: 50,
+                          ),
+                        ),
+                        trailing: CupertinoSwitch(
+                          value: true,
+                          onChanged: (newvalue) {
+
+                          },
+                        ),
+                    ),
+                    Divider(color: Colors.grey,height: 0,)
+                  ],
+                );
+              },
+              childCount: alarmList.length)
+          ),
+        ],
       ),
     );
   }
